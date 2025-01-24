f71e04bbf37636d5fde0e6cfdfee8e1cf969c8e6
Hass <hassmroueh@gmail.com>
2025-01-23 22:13:01 -0500

    authorization lab complete

--- server/app.py
+++ server/app.py
@@ -83,16 +83,21 @@ class CheckSession(Resource):
             return user.to_dict(), 200
         
         return {}, 401
-
-class MemberOnlyIndex(Resource):
+@app.before_request
+def check_if_logged_in():
+    if request.endpoint in ['member_index','member_article'] and not session['user_id']:
+        return {'Error' : 'Unauthorized'}, 401
     
+class MemberOnlyIndex(Resource):
     def get(self):
-        pass
-
+        articles = [article.to_dict() for article in Article.query.filter_by(is_member_only = True).all()]
+        return make_response(articles,200)
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        article = Article.query.filter_by(id=id, is_member_only = True ).first()
+        if article:
+            return make_response(article.to_dict(),200)
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
